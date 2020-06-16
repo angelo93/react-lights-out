7eb725893613ad02449c75e5078c9a169b7a9861
Michael Navarro <navarro.michael93@gmail.com>
2020-06-15 20:26:36 -0500

    Updates

---
+++ precache-manifest.ca615dff171aabc6088edfe718d33b2f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "882e68e0cd94876c989c",
+    "url": "/react-lights-out/static/css/main.9681784e.chunk.css"
+  },
+  {
+    "revision": "882e68e0cd94876c989c",
+    "url": "/react-lights-out/static/js/main.882e68e0.chunk.js"
+  },
+  {
+    "revision": "45ea534042495dbb2393",
+    "url": "/react-lights-out/static/js/1.45ea5340.chunk.js"
+  },
+  {
+    "revision": "1764a79bcd54ed66a092",
+    "url": "/react-lights-out/static/js/runtime~main.1764a79b.js"
+  },
+  {
+    "revision": "272dc670938b2f867c224b8f20a8142d",
+    "url": "/react-lights-out/index.html"
+  }
+];
